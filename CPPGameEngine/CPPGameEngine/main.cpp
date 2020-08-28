854a4296c117e07773ec1c9e56f07a515e63089f
Chaitanya Kochhar <ckochhar@riotgames.com>
2020-08-28 13:48:55 -0700

    Created project in Xcode

---
+++ CPPGameEngine/CPPGameEngine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPPGameEngine
+//
+//  Created by Chaitanya Kochhar on 8/28/20.
+//  Copyright © 2020 Chaitanya Kochhar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
